18b2b1894f1b6ef974234f9db265c80a595cacf5
Francesco Avallone <f.avallone20@studenti.unisa.it>
2021-11-02 13:09:28 +0100

    it works

--- forward_chain.m
+++ forward_chain.m
@@ -1,13 +1,15 @@
-function u1  = forward_chain(in)
+function u  = forward_chain(in)
 %CHAIN Summary of this function goes here
 %   Detailed explanation goes here
 m = 100
 J = 10
 l = 1
 g= 9.81
-x1 = in(1)
-v = in(2)
-LgLf = l*sin(x1)/(m*l+J-m*l^2*(sin(x1))^2)
-u1 = v*1/LgLf;
+v = in(1)
+x1 = in(2)
+LgLf = l*sin(x1)/(m*l^2+J-m*l^2*(sin(x1))^2)
+u1 = v/LgLf;
+u2 = m*g*l*cos(x1)/(l*sin(x1))
+u = u1+u2
 end
 
--- linear_controller.m
+++ linear_controller.m
@@ -1,4 +1,4 @@
-function v = linear_controller(in)
+function out = linear_controller(in)
 %LINEAR_CONTROLLER Summary of this function goes here
 %   Detailed explanation goes here
 ydot = in(1)
@@ -6,7 +6,8 @@ y =in(2)
 yd_dotdot= in(3)
 yd_dot=in(4)
 yd = in(5)
-kp = -100
+kp = -10
 v = kp*(y-yd)+kp*(ydot-yd_dot)+yd_dotdot;
+out = [v; y]
 end
 
